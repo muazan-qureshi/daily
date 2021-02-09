0c02b53ece749133ef7da04b9d5b26f3df411623
Muazan Qureshi <61580773+muazan-qureshi@users.noreply.github.com>
2021-02-09 21:06:27 +0500

    Add files via upload

--- signup_login/signup_login.sql
+++ signup_login/signup_login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 08, 2021 at 06:54 PM
+-- Generation Time: Feb 09, 2021 at 05:02 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -37,7 +37,10 @@ CREATE TABLE `email` (
 --
 
 INSERT INTO `email` (`id`, `mail`) VALUES
+(45, 'ahmedmuazan1@gmail.com'),
+(52, 'ali@yahoo.vcom'),
 (43, 'hello@najhkjdfg.vom'),
+(50, 'irfan@mail.xom'),
 (1, 'muazan.web@gmail.com'),
 (2, 'muazan@mail.com'),
 (3, 'muazanqureshi3@gmail.com'),
@@ -66,8 +69,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `gender`, `dob`, `phone`) VALUES
-(1, 'Muazan Qureshi', 'muazan.web@gmail.com', '12101', 'Male', '2001-07-05', '2147483647'),
-(2, 'Alexa Grace', 'alexa@mail.com', '55', 'Female', '1995-05-05', '923120210855');
+(1, 'Muazan Qureshi', 'muazanqureshi3@gmail.com', '@#Philips572001', 'Male', '2001-07-05', '923010301642'),
+(2, 'wedding script', 'weddingscript@yahoo.com', '$898989Ml', 'Female', '1995-07-08', '923120210482');
 
 -- --------------------------------------------------------
 
@@ -106,7 +109,8 @@ ALTER TABLE `email`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `users1`
@@ -123,7 +127,7 @@ ALTER TABLE `users1`
 -- AUTO_INCREMENT for table `email`
 --
 ALTER TABLE `email`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT for table `users`
